02faede6bfabef95ced0bfaa69542a3c33febbe7
Mide Moore <olumide.moore2817@gmail.com>
2025-07-08 13:57:33 +0100

    Updated wrong username to email

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post("/login", async (req, res) => {
   try {
     //check db if email exists
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong User Name");
+    !user && res.status(401).json("Wrong Email");
     //if email exists, decrypt stored password
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
